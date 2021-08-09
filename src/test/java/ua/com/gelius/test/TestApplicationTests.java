acd9e70a202146c08a96e03cd9d813cc681aa55f
aleksey <ivankov.aleksey.v@gmail.com>
2021-08-09 14:02:01 +0300

    Init project

---
+++ src/test/java/ua/com/gelius/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.gelius.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
