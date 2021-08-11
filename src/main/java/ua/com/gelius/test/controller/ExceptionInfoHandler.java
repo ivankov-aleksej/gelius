12ca950cdc0561dcb35a09fb6b52e2a3154cc2d7
aleksey <ivankov.aleksey.v@gmail.com>
2021-08-11 14:50:37 +0300

    extension info exception handler

--- src/main/java/ua/com/gelius/test/controller/ExceptionInfoHandler.java
+++ src/main/java/ua/com/gelius/test/controller/ExceptionInfoHandler.java
@@ -3,13 +3,14 @@ package ua.com.gelius.test.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import ua.com.gelius.test.exception.ErrorInfo;
 import ua.com.gelius.test.exception.NotFoundException;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
-import static ua.com.gelius.test.exception.ErrorType.DATA_NOT_FOUND;
+import static ua.com.gelius.test.exception.ErrorType.*;
 
 @RestControllerAdvice
 public class ExceptionInfoHandler {
@@ -18,5 +19,17 @@ public class ExceptionInfoHandler {
     public ResponseEntity<ErrorInfo> handleGenericNotFoundException(HttpServletRequest req, NotFoundException e) {
         ErrorInfo error = new ErrorInfo(LocalDateTime.now(), DATA_NOT_FOUND.getStatus().value(), DATA_NOT_FOUND.getErrorMessage(), e.getMessage(), req.getRequestURI());
         return new ResponseEntity<>(error, DATA_NOT_FOUND.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorInfo> handleGenericException(HttpServletRequest req, MethodArgumentTypeMismatchException e) {
+        ErrorInfo error = new ErrorInfo(LocalDateTime.now(), BAD_REQUEST.getStatus().value(), BAD_REQUEST.getErrorMessage(), e.getMessage(), req.getRequestURI());
+        return new ResponseEntity<>(error, BAD_REQUEST.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorInfo> handleGenericException(HttpServletRequest req, Exception e) {
+        ErrorInfo error = new ErrorInfo(LocalDateTime.now(), APP_ERROR.getStatus().value(), APP_ERROR.getErrorMessage(), e.getMessage(), req.getRequestURI());
+        return new ResponseEntity<>(error, APP_ERROR.getStatus());
     }
 }
--- src/main/java/ua/com/gelius/test/exception/ErrorType.java
+++ src/main/java/ua/com/gelius/test/exception/ErrorType.java
@@ -3,7 +3,10 @@ package ua.com.gelius.test.exception;
 import org.springframework.http.HttpStatus;
 
 public enum ErrorType {
+    APP_ERROR("Application error", HttpStatus.INTERNAL_SERVER_ERROR),
+    BAD_REQUEST("Bad request", HttpStatus.BAD_REQUEST),
     DATA_NOT_FOUND("Data not found", HttpStatus.NOT_FOUND);
+
 
     private final String errorMessage;
     private final HttpStatus status;
